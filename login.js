c6c39b64a4dbb728d6b7b26491dba3435ee8ded0
julia lima <eduarda.julia@alunoescolassociais.org.br>
2023-07-05 10:17:18 -0300

    login

--- login.js
+++ login.js
@@ -1,13 +1,35 @@
-//Bloco de verificação de preenchimento de campos
+/* FUNÇÃO PARA VERIFICAR PREENCHIMENTO DE LOGIN*/
+function acessar() {
+    let loginEmail = document.getElementById("loginEmail").value;
+    let loginSenha = document.getElementById("loginSenha").value;
+ 
+    if(!loginEmail || !loginSenha) {
+        alert("favor preencha os campos para acessar o site");
+    } else {
+       window.location.href = "aula2.html";
+    }
+}
+/*FUNÇÃO PARA CRIAR A LISTA DE USUÁRIOS */
+/*
+--->Cria o Array para armazenar os usuários
+*/
+var dadosLista = [];
+function salvarUser(){
 
-function Verifica(){
-    let user = document.getElementById('email').value;
-    let senha = document.getElementById('senha').value;
-    
-    if(!user || !senha){
-        alert("Campos de preenchimento obrigatório. Favor preencher!");
-    }else{
-        alert("Campos preenchidos com sucesso!");
+    let nomeUser = document.getElementById("nomeUser").value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        criarLista();
+        console.log(dadosLista);
+    }
+    function criarLista(){
+        let tabela = document.getElementById ("tabela").innerHTML = "<tr><th>Nome usuário</th><th>Ações</th></tr>";
+
+        for(let i=0; i <= (dadosLista.length-1); i++){
+            tabela+= "<tr><td>" +dadosLista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger' onclick=''> Excluir</button></td></tr>";
+            document.getElementById('tabela').innerHTML = tabela;
+        }
     }
 
 }
